97f222ee064af79df54095ab1127440f8c7a1fae
Steven Lindberg <stevenjohnlindberg@gmail.com>
2017-02-06 00:15:34 -0800

    Modified test script to write to output file

--- gears.jl
+++ gears.jl
@@ -14,28 +14,31 @@ methods = [
   "A*" => a_star_search
 ]
 
-for ω_out = [ -117.0, 77.0, 377.0, -20.0, -2345.0, 2.0 ]
-  params = GearSearchParams(gears, ω_in, ω_out, ϵ_threshold, mesh_distance)
-  options = [ :max_depth => max_depth ]
+let io = STDOUT
+# open("results.out", "w") do io
+  for ω_out = [ -117.0, 77.0, 377.0, -20.0, -2345.0, 2.0 ]
+    params = GearSearchParams(gears, ω_in, ω_out, ϵ_threshold, mesh_distance)
+    options = [ :max_depth => max_depth ]
 
-  @printf "Target: %.0f -> %.0f\n\n" ω_in ω_out
+    @printf io "Target: %.0f -> %.0f\n\n" ω_in ω_out
 
-  for (name, method) in methods
-    (result, seconds, bytes) = @timed method(params; options...)
+    for (name, method) in methods
+      (result, seconds, bytes) = @timed method(params; options...)
 
-    goal = result.goal
-    @printf("* Method           : %s\n", name)
-    if goal != nothing
-      @printf "* Goal             : %s\n" goal
-      @printf "* ω out            : %.2f\n" ω_in * goal.gear_ratio
-      @printf "* Error            : %.2f%%\n" 100.0 * calc_error(goal.gear_ratio, params)
-      @printf "* Transition Cost  : %s\n" goal.total_teeth
-    else
-      println("* Goal             : not found")
+      goal = result.goal
+      @printf io "* Method           : %s\n" name
+      if goal != nothing
+        @printf io "* Goal             : %s\n" goal
+        @printf io "* ω out            : %.2f\n" ω_in * goal.gear_ratio
+        @printf io "* Error            : %.2f%%\n" 100.0 * calc_error(goal.gear_ratio, params)
+        @printf io "* Transition Cost  : %s\n" goal.total_teeth
+      else
+        @printf io "* Goal             : not found\n"
+      end
+      @printf io "* Nodes Evaluated  : %d\n" result.nodes_visited
+      @printf io "* Branching Factor : %.2f\n" result.branches / result.parent_nodes
+      @printf io "* Elapsed Time     : %.5fs\n" seconds
+      @printf io "* Memory Usage     : %.0f%s\n\n" bytes/(bytes >= 1e6 ? 1e6 : 1e3) bytes >= 1e6 ? 'M' : 'K'
     end
-    @printf "* Nodes Evaluated  : %d\n" result.nodes_visited
-    @printf "* Branching Factor : %.2f\n" result.branches / result.parent_nodes
-    @printf "* Elapsed Time     : %.5fs\n" seconds
-    @printf "* Memory Usage     : %.0f%s\n\n" bytes/(bytes >= 1e6 ? 1e6 : 1e3) bytes >= 1e6 ? 'M' : 'K'
   end
 end
